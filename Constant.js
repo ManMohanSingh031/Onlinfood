045b947220d7c7ba2fc6335abb8006b93bdc57b3
ManMohanSingh031 <manmohansingh8422@gmail.com>
2024-08-18 11:36:26 +0530

    update

--- Constant.js
+++ Constant.js
@@ -1,14 +1,14 @@
 //Constant.js
 
 // Card API
-export const IMG_CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+export const IMG_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_400,h_300/";
 
 export const Swiggy_API_URL = "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
 
 export const numberOfShimmerCards = 15;
 
 // Menu API
-export const ITEM_IMG_CDN_URL =  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
+export const ITEM_IMG_CDN_URL =  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const Swiggy_MENU_API_URL = "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
 
